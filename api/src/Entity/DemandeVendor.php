6b8456de4d979ab76d03e56ff253c3addfb83d6b
francisco.leblanc.sacha <francisco.leblanc.sacha@gmail.com>
2023-01-20 17:19:28 +0100

    les demandes sont ajouté à la bdd/form fonctionne

--- api/src/Entity/DemandeVendor.php
+++ api/src/Entity/DemandeVendor.php
@@ -30,6 +30,21 @@ class DemandeVendor
     #[ORM\Column(nullable: true)]
     private ?bool $accepted = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adress = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $postalcode = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $phonenumber = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -67,6 +82,66 @@ class DemandeVendor
     public function setAccepted(bool $accepted): self
     {
         $this->accepted = $accepted;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getPostalcode(): ?string
+    {
+        return $this->postalcode;
+    }
+
+    public function setPostalcode(string $postalcode): self
+    {
+        $this->postalcode = $postalcode;
+
+        return $this;
+    }
+
+    public function getPhonenumber(): ?string
+    {
+        return $this->phonenumber;
+    }
+
+    public function setPhonenumber(string $phonenumber): self
+    {
+        $this->phonenumber = $phonenumber;
 
         return $this;
     }
--- front/src/views/UpgradeVendorView.vue
+++ front/src/views/UpgradeVendorView.vue
@@ -1,95 +1,82 @@
 <template>
-    <form @submit.prevent="upgradeVendor(idUser)" class="flex flex-col gap-5">
+    <form @submit.prevent="askUpgradeVendor($event)" class="flex flex-col gap-5">
         <div>
-            <label for="lastname" class="block text-sm font-medium text-white"
-                >Nom</label
-            >
+            <label for="lastname" class="block text-sm font-medium text-white">Nom</label>
             <div class="mt-1">
-                <input
-                    type="text"
-                    name="lastname"
-                    id="lastname"
+                <input type="text" name="lastname" id="lastname"
                     class="block w-full rounded-md border-gray-300 text-gray-800 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Nom"
-                />
+                    placeholder="Nom" />
             </div>
         </div>
         <div>
-            <label for="firstname" class="block text-sm font-medium text-white"
-                >Prénom</label
-            >
+            <label for="firstname" class="block text-sm font-medium text-white">Prénom</label>
             <div class="mt-1">
-                <input
-                    type="text"
-                    name="firstname"
-                    id="firstname"
+                <input type="text" name="firstname" id="firstname"
                     class="block w-full rounded-md border-gray-300 text-gray-800 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Prénom"
-                />
+                    placeholder="Prénom" />
             </div>
         </div>
         <div>
-            <label for="adress" class="block text-sm font-medium text-white"
-                >Adresse</label
-            >
+            <label for="adress" class="block text-sm font-medium text-white">Adresse</label>
             <div class="mt-1">
-                <input
-                    type="text"
-                    name="adress"
-                    id="adress"
+                <input type="text" name="adress" id="adress"
                     class="block w-full rounded-md border-gray-300 text-gray-800 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Adresse"
-                />
+                    placeholder="Adresse" />
             </div>
         </div>
         <div>
-            <label for="cp" class="block text-sm font-medium text-white"
-                >Code postale</label
-            >
+            <label for="cp" class="block text-sm font-medium text-white">Code postale</label>
             <div class="mt-1">
-                <input
-                    type="text"
-                    name="cp"
-                    id="cp"
+                <input type="text" name="cp" id="cp"
                     class="block w-full rounded-md border-gray-300 text-gray-800 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                    placeholder="Code postale"
-                />
+                    placeholder="Code postale" />
             </div>
         </div>
         <div>
-            <label for="tel" class="block text-sm font-medium text-white"
-                >N° de Téléphone</label
-            >
+            <label for="tel" class="block text-sm font-medium text-white">N° de Téléphone</label>
             <div class="mt-1">
-                <input
-                    type="tel"
-                    name="tel"
-                    id="tel"
+                <input type="tel" name="tel" id="tel"
                     class="block w-full rounded-md border-gray-300 text-gray-800 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                    placeholder="N° de Téléphone"
-                />
+                    placeholder="N° de Téléphone" />
             </div>
         </div>
-        <button
-            type="submit"
-            class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        >
+        <button type="submit"
+            class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
             Envoyer la demande
         </button>
     </form>
 </template>
 
 <script setup>
+import { ENTRYPOINT } from "../../config/entrypoint";
 import { useUserStore } from "../stores/user";
+import jwtDecode from "jwt-decode";
 
-async function upgradeVendor(event) {
+async function askUpgradeVendor(event) {
     const formData = new FormData(event.target);
-    const store = useUserStore();
-    await store.register(
-        formData.get("email"),
-        formData.get("name"),
-        formData.get("password")
-    );
-    console.log(store.token);
-}
+    let decoded = jwtDecode(localStorage.getItem("token"));
+    console.log(decoded.id);
+    // PUT request using fetch with async/await
+    const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            whoUser: `api/users/${decoded.id}`,
+            type: "vendeur",
+            firstname: formData.get("firstname"),
+            lastname: formData.get("lastname"),
+            adress: formData.get("adress"),
+            postalcode: formData.get("cp"),
+            phonenumber: formData.get("tel")
+        })
+    };
+
+    console.log(requestOptions.body);
+    console.log(ENTRYPOINT + `/demande_vendors`);
+    const response = await fetch(ENTRYPOINT + `/demande_vendors`, requestOptions);
+    const data = await response.json();
+    console.log(data.id);
+    if (data.id) {
+        alert("Votre demande a bien été prise en compte");
+    }
 </script>
