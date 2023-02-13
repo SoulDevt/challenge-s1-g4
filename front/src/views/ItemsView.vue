4b505f3fc0ab1ca61f52e94bc736844676ce1f3f
nikola panic <npanic@myges.fr>
2023-02-13 20:51:19 +0100

    fix button pay

--- front/src/views/ItemsView.vue
+++ front/src/views/ItemsView.vue
@@ -25,12 +25,6 @@
             </div>
         </div>
     </div>
-    <div>
-        <button @click="buy"
-            class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-            Pay now!
-        </button>
-    </div>
 </template>
 
 <script setup>
