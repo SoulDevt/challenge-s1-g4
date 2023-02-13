b1011e28f07b3fd6503bf6ed55b5f7f1ab2f84c3
Mehdi Saber <m1d3s@protonmail.com>
2023-02-13 19:13:18 +0100

    WIP CICD

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -41,9 +41,10 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 			echo "The database is now ready and reachable"
 		fi
 
-		if [ "$( find ./migrations -iname '*.php' -print -quit )" ]; then
-			php bin/console doctrine:migrations:migrate --no-interaction
-		fi
+#		if [ "$( find ./migrations -iname '*.php' -print -quit )" ]; then
+#			php bin/console doctrine:migrations:migrate --no-interaction
+#		fi
+		php bin/console doctrine:schema:update --force --no-interaction
 
 		if [ "$APP_ENV" != 'prod' ]; then
 			echo "Load fixtures"
