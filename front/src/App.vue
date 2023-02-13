c045db6a2cf5775be405ecb385027333d5cfd426
SoulDevt <souleymane.gueye.pro@gmail.com>
2023-02-13 21:43:08 +0100

    desgin front

--- front/src/App.vue
+++ front/src/App.vue
@@ -2,9 +2,11 @@
 import { RouterView } from "vue-router";
 import HeaderComponent from "./components/HeaderComponent.vue";
 import NavBar from "./components/NavBar.vue";
+import FooterComponent from "./components/FooterComponent.vue";
 </script>
 
 <template>
     <NavBar />
     <RouterView />
+    <FooterComponent />
 </template>
